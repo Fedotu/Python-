ecacd61c30c4048827dbf7f74e805b9a14c1a994
kc6mu8 <fedotu.01@gmail.com>
2026-01-23 12:22:41 +0400

    Изменеия в домашней работе lesson6

--- lesson6/Homework/lesson06_test3.py
+++ lesson6/Homework/lesson06_test3.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.get(
     "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-award_img = WebDriverWait(driver, 10).until(
-    lambda d: d.find_element(By.ID, "award")
-)
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "landscape")))
 
+award_img = driver.find_element(By.ID, "award")
 print(award_img.get_attribute("src"))
+
 driver.quit()
